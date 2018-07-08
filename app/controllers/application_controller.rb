13fb741e05f21aca0b5f5d1cb068e2a8692a4b7c
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-08 20:31:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ require "./config/environment"
 require 'sinatra/base'
 require 'rack-flash'
 
-enable :sessions
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  enable :sessions
 
   get '/' do
     erb :index
