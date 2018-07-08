496db861314f8ecaf68cc3cba3b07ad647e85493
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-08 19:45:36 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,24 +1,24 @@
 class SongsController < ApplicationController
 
-  get '/songs' do
-    @songs = Song.all
-    erb :'songs/index'
-  end
-
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'songs/show'
-  end
+  # get '/songs' do
+  #   @songs = Song.all
+  #   erb :'songs/index'
+  # end
+  # 
+  # get '/songs/:slug' do
+  #   @song = Song.find_by_slug(params[:slug])
+  #   erb :'songs/show'
+  # end
 
   get '/songs/new' do
     erb :'songs/new'
   end
 
-  post '/songs' do
-    @song = Song.create(params[:song])
-    @song.artist = Artist.find_or_create_by(name: params["artist"]["name"])
-    @song.genre_ids = params[:genres]
-    @song.save
-    redirect to "songs/#{@songs.slug}"
-  end
+  # post '/songs' do
+  #   @song = Song.create(params[:song])
+  #   @song.artist = Artist.find_or_create_by(name: params["artist"]["name"])
+  #   @song.genre_ids = params[:genres]
+  #   @song.save
+  #   redirect to "songs/#{@songs.slug}"
+  # end
 end
